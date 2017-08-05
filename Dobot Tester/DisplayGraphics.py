6fac677cd4fdc93f98fc80ce77cffc2958503246
Jimmy <jimmy.salies@etu.emse.fr>
2017-08-04 23:05:58 -0400

    modified : ajout de l'echelonnage relatif à la taille pour les graphiques'

--- Dobot Tester/DisplayGraphics.py
+++ Dobot Tester/DisplayGraphics.py
@@ -9,11 +9,17 @@ import pandas as pd
 
 def graphicsCalculs(values):
     deb=time.time()
-    if values[2]==1:
-        pas=5
+    data = pd.read_csv("./ressources/graph/"+values[0], skiprows=1)['Power']
+    if os.stat("./ressources/graph/"+values[0])[6]/1000000.>20:
+        pas=25
     else:
         pas=1
-    data = pd.read_csv("./ressources/graph/"+values[0], skiprows=1)['Power']
+    """
+    if values[2]==1:
+        pas=25
+    else:
+        pas=1
+    """
     data = scipy.signal.savgol_filter(data[0:len(data):pas], values[1], 3)
     print(values[0]+" : "+str(round(time.time()-deb,2))+"secs")
     return data
@@ -33,7 +39,14 @@ class graphics():
             multiListe.append([file,self.smoothingValues,self.oscillotype])
         multiprocessing=Pool(4)
         self.resultat = multiprocessing.map(graphicsCalculs, multiListe)
+        LenMin=len(self.resultat[0])
         for curve in self.resultat:
+            if LenMin>len(curve):
+                LenMin=len(curve)
             plt.plot(curve)
+        Moyenne=LenMin*[0]
+        for curve in self.resultat:
+            Moyenne+=curve[0:LenMin]
+        plt.plot(Moyenne/len(self.resultat),"b:",linewidth=3)
         print("durée total de la génération de graph : " + str(round(time.time() - deb, 2)) + "secs")
         plt.show()
